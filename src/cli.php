9a95511cc4dac6a4f268a9830f60f81451c72848
kos <konstantin000008@gmail.com>
2019-07-03 14:06:07 +0300

    step 3. linter without mistakes

--- src/cli.php
+++ src/cli.php
@@ -2,8 +2,9 @@
 namespace  BrainGames\Cli;
 use function \cli\line;
 
-function run() {
-  line('Welcome to the Brain Game!');
-  $name = \cli\prompt('May I have your name?');
-     line ("Hello, %s!", $name);
-  }
+function run()
+{
+    line('Welcome to the Brain Game!');
+    $name = \cli\prompt('May I have your name?');
+     line("Hello, %s!", $name);
+}
